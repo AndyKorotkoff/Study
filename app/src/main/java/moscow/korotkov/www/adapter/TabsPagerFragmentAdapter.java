b71952217da7c82c9edc780b7413e07f070e9dbf
AndyKorotkoff <andrey@korotkov.moscow>
2016-02-19 13:32:23 +0300

    1234567

--- app/src/main/java/moscow/korotkov/www/adapter/TabsPagerFragmentAdapter.java
+++
@@ -1,2 +0,0 @@
-package moscow.korotkov.www.adapter;
-
---
+++ app/src/main/java/moscow/korotkov/www/fragment/Fragment1.java
@@ -0,0 +1,9 @@
+package moscow.korotkov.www.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by root on 19.02.16.
+ */
+public class Fragment1 extends Fragment {
+}
