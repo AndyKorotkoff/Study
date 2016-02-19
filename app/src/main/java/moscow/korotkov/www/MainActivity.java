3248ab5d26baabae9f76a9a3c08909e400c310b5
AndyKorotkoff <andrey@korotkov.moscow>
2016-02-19 12:21:38 +0300

    123

---
+++ app/src/main/java/moscow/korotkov/www/MainActivity.java
@@ -0,0 +1,50 @@
+package moscow.korotkov.www;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by root on 18.02.16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private TabLayout tabLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Default);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+        initNawigationView();
+        tabLayout = (TabLayout)findViewById(R.id.tablayout);
+    }
+
+
+    private void initToolbar() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+
+    }
+    private void initNawigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+}
---
+++ app/src/main/java/moscow/korotkov/www/adapter/TabsPagerFragmentAdapter.java
@@ -0,0 +1,2 @@
+package moscow.korotkov.www.adapter;
+
