61437655eb284bab14ac37860899dd512b666bd1
AndyKorotkoff <andrey@korotkov.moscow>
2016-02-19 13:31:43 +0300

    123456

--- app/src/main/java/moscow/korotkov/www/adapter/TabsPagerFragmentsAdapter.java
+++ app/src/main/java/moscow/korotkov/www/adapter/TabsPagerFragmentsAdapter.java
@@ -1,7 +1,44 @@
 package moscow.korotkov.www.adapter;
 
-/**
- * Created by root on 19.02.16.
- */
-public class TabsPagerFragmentsAdapter {
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerFragmentsAdapter extends FragmentPagerAdapter {
+
+    String[] tabs;
+
+    public TabsPagerFragmentsAdapter(FragmentManager fm) {
+        super(fm);
+        tabs = new String[]{
+                "tab1",
+                "tab2",
+                "tab3"
+
+        };
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
 }
